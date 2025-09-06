7ca5adf26dd8862d013f4cc76193410afaba9b24
larissahsilva <larissah_santos2010@hotmail.com>
2025-09-06 14:34:24 -0300

    feat: iniciando projeto

---
+++ src/test/java/br/com/fiap/qhealth/atendimentoreceiver/AtendimentoReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.qhealth.atendimentoreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
